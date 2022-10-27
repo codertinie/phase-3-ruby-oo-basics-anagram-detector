d667ff87e370a877e8b5e77b71a6c2961ee3dbc2
codertinie <martin.munyao@moringaschool.com>
2022-10-27 21:58:44 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram)
+    anagram.filter {|word| word.chars.sort == @word.chars.sort}
+  end
+end
